2827784c6e269435ab4d0eb7700f06ac8d0c87da
Ankit-Khachane <khachane.ankit417@gmail.com>
2019-12-07 14:57:35 +0530

    optimized ui elements.

--- app/src/main/java/com/jforce/voting/activity/SplashActivity.java
+++ app/src/main/java/com/jforce/voting/activity/SplashActivity.java
@@ -1,16 +1,32 @@
 package com.jforce.voting.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.jforce.voting.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = "SplashActivity";
+    private static final long SPLASH_TIMEOUT = 2000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+
     }
 }
